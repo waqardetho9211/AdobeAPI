ad0892be37f049dbb09204d9694069acc5d8e52f
Waqar Detho <waqaralidetho@hotmail.com>
2019-01-22 20:26:59 +0100

    Connect Atlas mongo DB through mongo campus
    - just tested the insert of a document through code.

--- src/main/persistence/mongo/Main.java
+++ src/main/persistence/mongo/Main.java
@@ -1,20 +1,25 @@
 package main.persistence.mongo;
 
-import com.example.config.MongoContext;
-import com.mongodb.DBCursor;
-/**
- * @author Shamik Mitra
- *
- */
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
-	/**
-	 *
-	 */
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String[] args) {
-		DBCursor result = MongoContext.get().connectDb("test").findByKey("Employee", "age", 32,
-				(value) -> new Integer(value));
-		while (result.hasNext()) {
-			System.out.println(result.next());
-		}
+		String uri = "mongodb+srv://admin:admin@cluster0-1ng4z.mongodb.net/admin";
+		MongoClientURI clientURI = new MongoClientURI(uri);
+		MongoClient mongoClient = new MongoClient(clientURI);
+
+		MongoDatabase mongoDatabase = mongoClient.getDatabase("AdobeAPI");
+		MongoCollection collection = mongoDatabase.getCollection("Resource");
+
+		System.out.println("Database Connected");
+		Document document = new Document("xx", "yy");
+
+		collection.insertOne(document);
 	}
 }
