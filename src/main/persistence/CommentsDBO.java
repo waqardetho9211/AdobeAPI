d5e61c9d48107c60df028e16e3d3f16a8ae1cf55
Waqar Detho <waqar.detho@otto.de>
2019-01-25 18:34:43 +0100

    refactor: Change security settings and access modifiers

--- src/main/persistence/CommentsDBO.java
+++ src/main/persistence/CommentsDBO.java
@@ -1,5 +1,8 @@
 package main.persistence;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,17 +23,22 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class CommentsDBO implements CommentsDAO{
-	MongoCollection<Comment> collection;
-	String uri = "mongodb+srv://admin:`dxefRRvbr36.3#UBRK\\tNh@cluster0-1ng4z.mongodb.net/admin";
+	private MongoCollection<Comment> collection;
+	private String uri = "mongodb+srv://admin:<password>@cluster0-1ng4z.mongodb.net/admin";
 
-	MongoClientURI clientURI = new MongoClientURI(uri);
-	//MongoClient mongoClient;
-	MongoClient mongoClient = new MongoClient(clientURI);
-	MongoDatabase mongoDatabase = mongoClient.getDatabase("AdobeAPI"); 
+	private MongoClientURI clientURI = new MongoClientURI(uri);
+	private MongoClient mongoClient;
+	//MongoClient mongoClient = new MongoClient(clientURI);
+	private MongoDatabase mongoDatabase;
+
+	{
+		mongoDatabase = mongoClient.getDatabase("AdobeAPI");
+		System.out.println("Print Logs")
+	}
 
 	public CommentsDBO() {
 
-		CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+		final CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
 				fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 		mongoClient = new MongoClient(uri,
 				MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
@@ -42,15 +50,15 @@ public class CommentsDBO implements CommentsDAO{
 
 	}
 	@Override
-	public void insertComment(Comment commentBO) {
+	public void insertComment(final Comment commentBO) {
 		collection.insertOne(new Comment(commentBO.getUser(), commentBO.getComment()));		
 	}
 
 	@Override
 	@SuppressWarnings("deprecation")
 	public List<Comment> getAllComments() {
-		List<Comment> result = new ArrayList<Comment>();
-		Block<Comment> printBlock = new Block<Comment>() {
+		final List<Comment> result = new ArrayList<Comment>();
+		final Block<Comment> printBlock = new Block<Comment>() {
 		    @Override
 		    public void apply(final Comment comment) {
 		    	result.add(comment);
