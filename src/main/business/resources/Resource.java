a4d76da4c55b07ec6b21986b79602772aeef5329
Waqar Detho <waqaralidetho@hotmail.com>
2019-01-26 20:53:04 +0100

    feat: Add hash code in Resource business object for etag

--- src/main/business/resources/Resource.java
+++ src/main/business/resources/Resource.java
@@ -1,6 +1,46 @@
 package main.business.resources;
 
 public class Resource {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Resource other = (Resource) obj;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
 	private String name;
 	private ResourceTypes type;
 	private String path;
