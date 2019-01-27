70b2f2c63399db69f337ed6d0d97f7d935d191de
Waqar Detho <waqaralidetho@hotmail.com>
2019-01-27 13:44:54 +0100

    fix: Add evaluate condition HTTP Caching

--- src/main/ResourcesController.java
+++ src/main/ResourcesController.java
@@ -3,9 +3,12 @@ package main;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.xml.bind.DatatypeConverter;
 
 import main.business.resources.Resource;
@@ -21,16 +24,21 @@ public class ResourcesController {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getResources() throws NoSuchAlgorithmException {
+	public Response getResources(@Context Request request) throws NoSuchAlgorithmException {
 		ResourceDBO dao = new ResourceDBO();
 		List<Resource> resources = dao.getAllResources();
-		
+
 		MessageDigest digest = MessageDigest.getInstance("MD5");
-        byte[] hash = digest.digest(resources.toString().getBytes(StandardCharsets.UTF_8));
-        String hex = DatatypeConverter.printHexBinary(hash);
-        EntityTag etag = new EntityTag(hex);
-        
-        return Response.ok(resources).tag(etag).build(); 
+		byte[] hash = digest.digest(resources.toString().getBytes(StandardCharsets.UTF_8));
+		String hex = DatatypeConverter.printHexBinary(hash);
+		EntityTag etag = new EntityTag(hex);
+
+		ResponseBuilder builder = request.evaluatePreconditions(etag);
+		if (builder != null) {
+			return builder.build();
+		}
+
+		return Response.ok(resources).tag(etag).build();
 	}
 
 }
