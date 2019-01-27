35eb11097808e5b7406b80e519d5067de41585e6
Waqar Detho <waqaralidetho@hotmail.com>
2019-01-27 13:44:18 +0100

    refactor: Refactor Resource controller

--- src/main/ResourceController.java
+++ src/main/ResourceController.java
@@ -21,37 +21,39 @@ import main.persistence.ResourceDBO;
 
 @Singleton
 @Path("/resource")
-public class ResourceController { 
+public class ResourceController {
 	private Resource resource;
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getResource(@QueryParam("name") final String name, @Context Request request) {
 		ResourceDAO resourceDAO = new ResourceDBO();
 		resource = resourceDAO.getAResource(name);
-		
+
 		if (resource == null) {
 			return Response.noContent().build();
 		}
 		EntityTag etag = new EntityTag(Integer.toString(resource.hashCode()));
 		ResponseBuilder builder = request.evaluatePreconditions(etag);
-        if (builder != null) {
-            return builder.build();
-        }
-        return Response.ok(resource).tag(etag).build();
+		if (builder != null) {
+			return builder.build();
+		}
+		return Response.ok(resource).tag(etag).build();
 	}
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response postResource(@FormParam("name") final String name, @FormParam("type") String type,
 			@FormParam("path") String path, @FormParam("location") String location, @Context Request response) {
-		
-		//Check condition if a resource already there. Since, method is a post method.
-		Resource resource = new Resource(name, ((type == null) ? ResourceTypes.Unknown : ResourceTypes.valueOf(type)), path, location);
+
+		// Check condition if a resource already there. Since, method is a post method.
+		Resource resource = new Resource(name, ((type == null) ? ResourceTypes.Unknown : ResourceTypes.valueOf(type)),
+				path, location);
 		ResourceDAO resourceDAO = new ResourceDBO();
 		resourceDAO.insertResource(resource);
-		
+
 		EntityTag etag = new EntityTag(Integer.toString(resource.hashCode()));
-		
+
 		return Response.ok(resource).tag(etag).build();
 	}
 }
